9286ff2f36f5231d322a061114a03142b1385ae1
KaiNgo <thuanngo2106@gmail.com>
2023-02-11 19:26:42 +0700

    - create project. - update .gitignore (don't update POD folder)

---
+++ FloodAreas/FloodAreas/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FloodAreas
+//
+//  Created by KaiNgo on 06/02/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ FloodAreas/FloodAreas/Modules/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloodAreas
+//
+//  Created by KaiNgo on 06/02/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("\(#function)")
+    }
+
+}
+
